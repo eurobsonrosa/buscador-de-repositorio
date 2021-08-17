c9b471f469e7745f972055402a4f62bd1850359d
Robson Rosa <contatorobsonrosa@gmail.com>
2021-08-16 23:03:36 -0300

    adicionando arquivos restantes

---
+++ src/Pages/Home/styled.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.div `
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+height: 100vh;
+`;
+
+export const Content = styled.div `
+width: 100vw;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
+
+export const Title = styled.h1`
+text-align: center;
+font-size: 2rem;
+font-family: sans-serif;
+color: #333;
+`
+
+export const Input = styled.input `
+border: 1px solid #ddd;
+height: 1.5rem;
+padding: 0 .5rem;
+border-radius: .25rem 0 0 .25rem;
+&: focus,
+&:active {
+    outile: none;
+    box-shadow: nome;
+}
+`;
+
+export const Button = styled.button `
+height: 1.6rem;
+border: 1px solid #000;
+background: #000;
+color: #fff;
+border-radius: 0 .25rem .25rem 0;
+
+&: focus,
+&:active {
+    outile: none;
+    box-shadow: nome;
+}
+`;
+
+export const ErrorMsg = styled.span `
+display: block;
+font-size: 0.8rem;
+color: red;
+font-weight: 600;
+margin-top: 1rem;
+`
---
+++ src/Pages/Repositories/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import * as S from './styled';
+import { useHistory } from 'react-router-dom';
+
+
+export default function Repositories() {
+    const history = useHistory();
+    const [repositories, setRepositories] = useState([]);
+    useEffect(() => {
+        let repositoriesName = localStorage.getItem('repositoriesName');
+        if(repositoriesName != null) {
+            repositoriesName = JSON.parse(repositoriesName);
+            setRepositories(repositoriesName);
+            localStorage.clear();
+        } else {
+            history.push('/');
+        }
+
+    }, []);
+    return (
+        <S.Container>
+            <S.Title>Repositórios</S.Title>
+            <S.List>
+                {repositories.map(repository => {
+                    return (
+                        <S.ListItem>Repositório: {repository}</S.ListItem>
+                    )
+                })}
+            </S.List>
+            <S.linkHome to="/">Voltar</S.linkHome>
+        </S.Container>
+    )
+}
+
